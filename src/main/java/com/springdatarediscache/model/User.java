b38bbb666d2fd0b9fc4f7e5a82875bb81ec8bc68
Nihad <nihadamirov98@gmail.com>
2024-08-28 23:06:56 +0400

    add entity file

---
+++ src/main/java/com/springdatarediscache/model/User.java
@@ -0,0 +1,25 @@
+package com.springdatarediscache.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String surname;
+    private int age;
+    private String email;
+}
